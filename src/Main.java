5f1ea348eda5bc61f35d89726d4fb3e8a9b6ad29
FERNANDO SANTOS <fernandopernambuco@gmail.com>
2024-11-13 14:29:06 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 
 public class Main {
+    public static void main(String[] args) {
+        // Criando um cliente e contas
+        Cliente cliente1 = new Cliente("Alice", "123.456.789-00", "alice@email.com");
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+        // Criando uma conta corrente
+        ContaCorrente contaCorrente = new ContaCorrente("CC123", 500);
+        cliente1.criarConta(contaCorrente);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        // Operações na conta corrente
+        cliente1.depositar(1000, "CC123");
+        cliente1.sacar(800, "CC123");
+        cliente1.consultarSaldo("CC123");
+        contaCorrente.cobrarTaxaManutencao();
+        cliente1.consultarSaldo("CC123");
 
+        // Criando uma conta poupança
+        ContaPoupanca contaPoupanca = new ContaPoupanca("PP456", 5);
+        cliente1.criarConta(contaPoupanca);
+
+        // Operações na conta poupança
+        cliente1.depositar(200, "PP456");
+        contaPoupanca.aplicarJuros(); // Aplicando juros na conta poupança
+        cliente1.consultarSaldo("PP456");
+    }
 }
