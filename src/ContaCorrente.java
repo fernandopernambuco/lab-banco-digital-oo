5930fd12e164fe02b4bce2cd6db430cb788a6d53
FERNANDO SANTOS <fernandopernambuco@gmail.com>
2024-11-13 14:28:16 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,14 +1,23 @@
 
 public class ContaCorrente extends Conta {
 
-	public ContaCorrente(Cliente cliente) {
-		super(cliente);
-	}
+    private static final double TAXA_MANUTENCAO = 10.0;
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Corrente ===");
-		super.imprimirInfosComuns();
-	}
-	
+    public ContaCorrente(String numero, double saldo) {
+        super(numero, saldo);
+    }
+
+    public void cobrarTaxaManutencao() {
+        if (saldo >= TAXA_MANUTENCAO) {
+            saldo -= TAXA_MANUTENCAO;
+            System.out.println("Taxa de manutenção de R$" + TAXA_MANUTENCAO + " cobrada com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para cobrança da taxa de manutenção.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ContaCorrente{" + "numero='" + numero + '\'' + ", saldo=" + saldo + '}';
+    }
 }
